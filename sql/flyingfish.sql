fbecd0f2ec3ca2be0e5cf04257d0a0b0d013588e
fanrenlangzi <1291321723@qq.com>
2016-06-21 11:36:36 +0800

    修改sql

--- sql/flyingfish.sql
+++ sql/flyingfish.sql
@@ -9,16 +9,16 @@ create table `video` (
     `classify_2`	varchar(30)		not null	default ''	comment '二级分类',
     `classify_3`	varchar(30)		not null	default ''	comment '三级分类',
     `classify_4`	varchar(30)		not null	default ''	comment '四级分类',
-    `link`			varchar(200)	not null	default ''	comment '视频链接',
+    `link`			varchar(50)		not null	default ''	comment '视频链接',
     `picture_url`	varchar(50)		not null	default ''	comment '缩略图地址',
-    `source`		varchar(50)		not null	default ''	comment '视频源',
-    `is_unique`		smallint		not null	default 0	comment '是否独播:1独播，0不是独播',
+    `source`		varchar(200)	not null	default ''	comment '视频源',
+    `is_unique`		tinyint			not null	default 0	comment '是否独播',
     `count`			int				not null	default 0	comment '播放量',
     `intro`			varchar(500)	not null	default ''	comment '简介',
     `start_time`	datetime		not null	default '1970-01-01 00:00:00'	comment '上映时间',
     `update_time`	datetime		not null	default '1970-01-01 00:00:00'	comment '更新时间',
-    `score`			smallint		not null	default 0	comment '评分',
-    `drama_num`		smallint		not null	default 1	comment	'剧集数',
+    `score`			tinyint			not null	default 0	comment '评分',
+    `drama_num`		tinyint			not null	default 1	comment	'剧集数',
     primary key (`id`),
-    unique  key (`link`, `source`)
+    unique  key (`name`)
 ) comment '视频信息表' default charset=utf8mb4;

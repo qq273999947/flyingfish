b8c01567427bd2bcaf3cdcbdbded344e83d4afb1
fanrenlangzi <15191432680@163.com>
2016-07-17 22:37:53 +0800

    update mysql

--- web_crawler/run/load_data.sh
+++ web_crawler/run/load_data.sh
@@ -1,6 +1,6 @@
 
 
-mysql -uroot -p0 -h182.254.139.172 -e "
+mysql -u${mysql_user} -p${mysql_passwd} -h${host} -e "
     use flyingfish;
     LOAD DATA LOCAL INFILE 'animelist_20160717.txt' into table video(
         name, classify_1, classify_2, classify_3, classify_4, link, picture_url, source, is_unique, count, intro, start_time, update_time, score, drama_num

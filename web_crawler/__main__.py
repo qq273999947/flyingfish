531cc4b74898e3d6c5e68445024fe98344d5bb7d
yuxiang.zhu <yuxiang.zhu@qunar.com>
2016-07-24 09:39:39 +0800

    实现多线程并行抓取

--- web_crawler/__main__.py
+++ web_crawler/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 
 from comm.comm import  date_str
 from parser.anime_parser import AnimeParser
@@ -10,18 +11,37 @@ logging.basicConfig(level=logging.DEBUG,
                     filename='log/video_parser_' + date_str + '.log',
                     filemode='w')
 
-def main():
+def parse_anime_video():
     # 获取动漫
     anime_parser = AnimeParser()
     anime_parser.parse_videos()
 
+def parse_teleplay_video():
     # 获取电视剧
     teleplay_parser = TeleplayParser()
     teleplay_parser.parse_videos()
 
+def parse_movie_video():
     # 获取电影
     movie_parser = MovieParser()
     movie_parser.parse_videos()
+
+def main():
+
+    anime_parser_thread = threading.Thread(target=parse_anime_video, name="AnimeParser")
+    teleplay_parser_thread = threading.Thread(target=parse_teleplay_video, name="TeleplayParser")
+    movie_parser_thread = threading.Thread(target=parse_movie_video, name="MovieParser")
+
+    anime_parser_thread.start()
+    teleplay_parser_thread.start()
+    movie_parser_thread.start()
+
+    anime_parser_thread.join()
+    logging.info('AnimeParser thread 执行完毕')
+    teleplay_parser_thread.join()
+    logging.info('TeleplayParser thread 执行完毕')
+    movie_parser_thread.join()
+    logging.info('MovieParser thread 执行完毕')
 
 if __name__ == '__main__':
     main()
